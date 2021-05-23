31e3fa5461e39193ec083023aa3c8fc15ecb80d5
Per <cph-pk171@cphbusiness.dk>
2021-05-23 19:41:56 +0200

    Altered message when logged in

--- src/main/java/cors/CorsFilter.java
+++ src/main/java/cors/CorsFilter.java
@@ -53,6 +53,6 @@ public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilt
     /* Cross origin requests can be either simple requests or preflight request. We need to add this
      header to both types of requests. Only preflight requests need the previously added headers. */
      response.getHeaders().add("Access-Control-Allow-Origin", "https://dat4semsecurity.surge.sh");
-//     response.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:3000");
+     //response.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:3000");
   }
 }
